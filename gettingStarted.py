0395025799fe7d440f82769af082aab48cf4e123
Zach Fleeman <ztf210@nyu.edu>
2026-09-12 11:30:47 -0400

    Update question/answers

--- gettingStarted.py
+++ gettingStarted.py
@@ -3,17 +3,28 @@
 ### Output - The right answer for the specific question.
 
 def welcome_assignment_answers(question):
-    #Students do not have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
-    else: 
-        ### you should understand why this else case should be included
-        ### what happens if there is a typo in one of the questions?
-        ### maybe put something here to flag an issue and catch errors
-        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+    match question:
+        case "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+            answer = "pcap"
+        case "Are encoding and encryption the same? - Yes/No":
+            answer = "No"
+        case "Is it possible to decrypt a message without a key? - Yes/No":
+            answer = "No"
+        case "Is it possible to decode a message without a key? - Yes/No":
+            answer = "No"
+        case "Is a hashed message supposed to be un-hashed? - Yes/No":
+            answer = "No"
+        case "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+            answer = "817cbceb6ca79341e1117e49dd49264aa2a2e19a2049105d59d0567eba06f4a3"
+        case "Is MD5 a secured hashing algorithm? - Yes/No":
+            answer = "No"
+        case "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+            answer = "7"
+        case "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+            answer = "3"
+        case _:
+            # Catch unrecognized questions, including typos.
+            answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
     return(answer)
 # Complete all the questions.
 
